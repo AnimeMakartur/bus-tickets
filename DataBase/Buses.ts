4f6100582fe8e0844f70c5afd2325a3bcb4aaa96
Shishkov Egor <131530870+AnimeMakartur@users.noreply.github.com>
2026-07-01 12:38:47 +0300

    Update Buses.ts

--- DataBase/Buses.ts
+++ DataBase/Buses.ts
@@ -4,5 +4,6 @@ export const busesArray: Bus[] = [
     new Bus(1, "Mersedes Bens Tourismo", "B001", 200),
     new Bus(2, "Volvo 9700", "B002", 180),
     new Bus(3, "Scania Touring", "B003", 220),
-    new Bus(4, "MAN Lion's Coach", "B004", 190)
+    new Bus(4, "MAN Lion's Coach", "B004", 190),
+    new Bus(5, "Setra S 531 DT", "B005", 210)
 ]
